2e8cb879f1b81eff6f94e6101d0e500c8d2e1a44
AGSCL <37744754+AGSCL@users.noreply.github.com>
2025-04-08 13:01:33 -0400

    aniadimos paquetes tar y aniadimos comentarios sobre historia de modelos de mezcla

---
+++ script_para_generar_paquetes_exportables_a_colab.R
@@ -0,0 +1,12 @@
+# Crear carpeta destino
+dir.create("paquetes_tar")
+
+# Lista de paquetes
+paquetes <- c("showtext", "ggplot2", "plotly", "grid", "gridExtra", "magick", "knitr",
+              "tidyverse", "rio", "psych", "parallel", "doParallel", "glca",
+              "DiagrammeR", "DiagrammeRsvg", "rsvg", "htmlwidgets", "janitor")
+
+# Descargar los paquetes en formato .tar.gz
+download.packages(paquetes, destdir = "paquetes_tar", type = "source")
+
+system("tar -czvf paquetes.tar.gz paquetes_tar")
